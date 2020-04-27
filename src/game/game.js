1be4b7f56b9b2481ae14ff7ee31d510a3e819107
CiocanGeorge <ciocan_george97@yahoo.com>
2020-04-27 21:01:27 +0300

    test garduri

--- src/game/game.js
+++ src/game/game.js
@@ -12,7 +12,7 @@ export default class Game {
     this.ctx = ctx;
     this.spacePressed = false;
     //this.fencesToGo = Game.FENCES_TO_WIN;
-    this.fencesToGo=10;
+    this.fencesToGo=5;
     //set the frames per second
     this.then = 0;
     this.fps = 10;
